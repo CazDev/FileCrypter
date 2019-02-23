58f5939aabb8540b42744af51c2459d778996d41
tavvi <tavvi.yt@gmail.com>
2019-02-23 17:52:55 +0300

    Added .gif file

--- FileCrypter/Crypter.cs
+++ FileCrypter/Crypter.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FileCrypter
 {
